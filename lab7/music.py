ebb539fd45d7bf431894568a875c906c3bc84e44
suunmiu <153590436+suunmiu@users.noreply.github.com>
2024-04-09 20:50:11 +0500

    Update music.py

--- lab7/music.py
+++ lab7/music.py
@@ -1,55 +1,59 @@
 import pygame
 
 pygame.init()
-pygame.mixer.init()
 
-def play_a_different_song():
-    global currently_playing_song, songs, current_song_index
-    next_song_index = (current_song_index + 1) % len(songs)
-    currently_playing_song = songs[next_song_index]
-    pygame.mixer.music.load(currently_playing_song)
+screen = pygame.display.set_mode((300, 300))
+pygame.display.set_caption("Music player")
+
+music_files = ["music/Beatles.mp3", "music/heaven.mp3", "music/Maria.mp3"]
+current_track = 0
+
+pygame.mixer.music.load(music_files[current_track])
+
+def play():
     pygame.mixer.music.play()
-    current_song_index = next_song_index
 
-def play_previous_song():
-    global currently_playing_song, songs, current_song_index
-    previous_song_index = (current_song_index - 1) % len(songs)
-    currently_playing_song = songs[previous_song_index]
-    pygame.mixer.music.load(currently_playing_song)
-    pygame.mixer.music.play()
-    current_song_index = previous_song_index
-
-def stop_music():
+def stop():
     pygame.mixer.music.stop()
 
-def play_pause_music():
-    if pygame.mixer.music.get_busy():
-        pygame.mixer.music.pause()
-    else:
-        pygame.mixer.music.unpause()
+def next_track():
+    global current_track
+    current_track = (current_track + 1) % len(music_files)
+    pygame.mixer.music.load(music_files[current_track])
+    play()
 
-window_width = 600
-window_height = 600
-window = pygame.display.set_mode((window_width, window_height))
-songs = ['1.mp3', '2.mp3','3.mp3']
-current_song_index = 0
-currently_playing_song = songs[current_song_index]
+def previous_track():
+    global current_track
+    current_track = (current_track - 1) % len(music_files)
+    pygame.mixer.music.load(music_files[current_track])
+    play()
 
-
-running = True
-while running:
+while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.KEYDOWN:
+            pygame.quit()
+            sys.exit()
+
+        elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
-                play_pause_music()
-            elif event.key == pygame.K_n:
-                play_a_different_song()
-            elif event.key == pygame.K_p:
-                play_previous_song()
-            elif event.key == pygame.K_s:
-                stop_music()
+                if pygame.mixer.music.get_busy():
+                    stop()
+                else:
+                    play()
+            elif event.key == pygame.K_RIGHT:
+                next_track()
+            elif event.key == pygame.K_LEFT:
+                previous_track()
+
+    screen.fill((255, 255, 255))
+
+    font = pygame.font.SysFont(None, 24)
+    text = font.render("Current Track: " + music_files[current_track], True, (0, 0, 0))
+    screen.blit(text, (10, 10))
+
+    pygame.display.update()
+
+pygame.quit()
 
 
     window.fill((255, 255, 255))
